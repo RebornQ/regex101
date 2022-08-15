50e3b1aba62e22aa0d92bef205a15ee1a9b0a93e
Reborn <ren_xiaoyao@outlook.com>
2022-08-15 20:29:15 +0800

    fix missing parse files

--- spy101.js
+++ spy101.js
@@ -17,18 +17,25 @@ const saveDir = path.resolve(__dirname, 'app');
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36';
 const domain = 'https://regex101.com';
 
-const instance = axios.create({ baseURL: domain, headers: { 'User-Agent': userAgent } });
+let axiosConfig = {
+  baseURL: domain,
+  headers: { 'User-Agent': userAgent }
+}
+if (typeof httpsAgent !== 'undefined') {
+  axiosConfig.httpAgent = httpsAgent;
+  axiosConfig.httpsAgent = httpsAgent;
+}
+const instance = axios.create(axiosConfig);
 
 const assetsCache = [];
 
 function parseSw(content) {
-  const matchs = content.match(/url:"\/.*?",/g);
-  return matchs.map((temp) => temp.replace(/(url:"|",)/g, ''));
+  const matches = content.match(/url:"\/.*?",/g);
+  return matches.map((temp) => temp.replace(/(url:"|",)/g, ''));
 }
 
 function parseIndex(content) {
-  const matchs = content.match(/\/static\/.*?(?=")/g);
-  return matchs;
+  return content.match(/\/static\/.*?(?=")/g);
 }
 
 async function spySw() {
@@ -53,7 +60,45 @@ async function spyIndex() {
   }
 }
 
+async function cacheMissParseFiles() {
+  assetsCache.push(
+    // PHP >= 7.3
+    '/static/pcre2.worker.0fff4fdbbf3423a854d7.js',
+    '/static/pcre2.worker.bb122b5b4907382062a6.js',
+    // PHP < 7.3
+    '/static/pcre.worker.5ad0361888bb2e998772.js',
+    '/static/generic.worker.e7ae3ee9e8b9b71f6a54.js',
+    '/static/libs/pcrelib.f84adbca8f2187ce1b565d5363317e69.js',
+    '/static/libs/pcrelib.879afe94f2f691ee64e38970bfdad0a9.wasm',
+    // JavaScript
+    '/static/javascript.worker.160b9eb5a8f6cc49b645.js',
+    '/static/javascript.worker.a10e707dccb487a4e5b9.js',
+    // Python
+    '/static/libs/pcrelib.f84adbca8f2187ce1b565d5363317e69.js',
+    '/static/libs/pcrelib.879afe94f2f691ee64e38970bfdad0a9.wasm',
+    // Golang
+    '/static/golang.worker.a56e4baf19f0745cf52d.js',
+    '/static/libs/golang.f6781daa95a91c37a657143af1c9fa15.js',
+    '/static/libs/golang.e823b447f564859c015c78baa62631e8.wasm',
+    // Java
+    '/static/java.worker.6657946717165202ea39.js',
+    '/static/java.worker.9b6147abd5e56893ed30.js',
+    '/static/libs/java8.8ebf0368cb4913100b222df85082da5e.js',
+    // .Net
+    '/static/dotnet.worker.50b8240687aed55fe54f.js',
+    '/static/dotnet.worker.2eb1850ee30f26cb12a6.js',
+    '/static/libs/dotnet.b34b811a67c7676a0512065d249fffe4.js',
+    '/static/libs/dotnet.514cb9edb4e6e83f02326c7305e6474a.wasm',
+    // Base
+    '/static/libs/pcre2lib.c3c3d3d340fea16a312b549130fa6564.js',
+    '/static/libs/pcre2lib.f2e2fa3bde3670b117a0d97bfa1be27c.wasm',
+    // 正则调试
+    '/static/vendors-sentry.0aec4123582cbb0e4dc0.chunk.js'
+  )
+}
+
 async function spyAssets() {
+  await cacheMissParseFiles()
   try {
     for (const filepath of assetsCache) {
       const resp = await instance.get(filepath, { responseType: 'stream' });
